b6da1902d437efde64c245647fca561fe129a444
Taner <Taner@DESKTOP-F23D3KC>
2018-03-12 21:22:28 +0000

    -added offers to shopping cart

--- src/main/java/ShoppingCart/ShoppingCart/ShoppingCart.java
+++ src/main/java/ShoppingCart/ShoppingCart/ShoppingCart.java
@@ -5,29 +5,43 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ShoppingCart {
 
 	private static Map<String, CartItem> itemMap;
-	private List<CartItem> cartItems;
+	private Map<CartItem, Long> cartItems;
+	private static Map<Class<? extends CartItem>, Integer> offers;
 
 	static {
 		itemMap = new HashMap<String, CartItem>();
 		itemMap.put("Apple", new Apple());
 		itemMap.put("Orange", new Orange());
+
+		offers = new HashMap<Class<? extends CartItem>, Integer>();
+		offers.put(Apple.class, 2);
+		offers.put(Orange.class, 3);
 	}
 
 	public ShoppingCart(List<String> items) {
-		cartItems = new ArrayList<CartItem>();
-		items.stream().filter(s -> itemMap.containsKey(s)).forEach(s -> cartItems.add(itemMap.get(s)));
+		cartItems = new HashMap<CartItem, Long>();
+		cartItems = items.stream().filter(s -> itemMap.containsKey(s))
+				.collect(Collectors.groupingBy(o -> itemMap.get(o), Collectors.counting()));
 
-		if (items.size() != cartItems.size()) {
+		if (items.size() != cartItems.entrySet().stream().mapToLong(entry -> entry.getValue()).sum()) {
 			throw new IllegalArgumentException();
 		}
 	}
 
 	public BigDecimal calculateCost() {
-		return cartItems.stream().map(s -> s.getPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		return cartItems.entrySet().stream().map(s -> s.getKey().getPrice().multiply(new BigDecimal(s.getValue())))
+				.reduce(BigDecimal.ZERO, BigDecimal::add).subtract(calculateDiscounts());
+	}
+
+	private BigDecimal calculateDiscounts() {
+		return cartItems.entrySet().stream().filter(s -> offers.containsKey(s.getKey().getClass())).map(
+				s -> s.getKey().getPrice().multiply(new BigDecimal(s.getValue() / (offers.get(s.getKey().getClass())))))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
 	}
 
 }
--- src/test/java/ShoppingCart/ShoppingCart/ShoppingCartTest.java
+++ src/test/java/ShoppingCart/ShoppingCart/ShoppingCartTest.java
@@ -13,15 +13,23 @@ public class ShoppingCartTest {
 	@Test
 	public void shouldCalculateCostOfShoppingCart() {
 		ShoppingCart cart = new ShoppingCart(Arrays.asList("Apple", "Apple", "Orange", "Apple"));
-		BigDecimal expectedCost = new BigDecimal(2.05).setScale(2, RoundingMode.HALF_UP);
+		BigDecimal expectedCost = new BigDecimal(1.45).setScale(2, RoundingMode.HALF_UP);
 
 		assertEquals(expectedCost, cart.calculateCost());
 	}
 
 	@Test
-	public void shouldCalculateCostOfShoppingCartWithOneItem() {
+	public void shouldCalculateCostOfShoppingCartWithOneAppleItem() {
 		ShoppingCart cart = new ShoppingCart(Arrays.asList("Apple"));
 		BigDecimal expectedCost = new BigDecimal(0.60).setScale(2, RoundingMode.HALF_UP);
+
+		assertEquals(expectedCost, cart.calculateCost());
+	}
+
+	@Test
+	public void shouldCalculateCostOfShoppingCartWithOneOrangeItem() {
+		ShoppingCart cart = new ShoppingCart(Arrays.asList("Orange"));
+		BigDecimal expectedCost = new BigDecimal(0.25).setScale(2, RoundingMode.HALF_UP);
 
 		assertEquals(expectedCost, cart.calculateCost());
 	}
@@ -29,6 +37,22 @@ public class ShoppingCartTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldThrowExceptionIfProductNotFound() {
 		new ShoppingCart(Arrays.asList("Apple", "Orange", "Banana"));
+	}
+
+	@Test
+	public void shouldCalculateAppleDiscount() {
+		ShoppingCart cart = new ShoppingCart(Arrays.asList("Apple", "Apple"));
+		BigDecimal expectedCost = new BigDecimal(0.60).setScale(2, RoundingMode.HALF_UP);
+
+		assertEquals(expectedCost, cart.calculateCost());
+	}
+
+	@Test
+	public void shouldCalculateOrangeDiscount() {
+		ShoppingCart cart = new ShoppingCart(Arrays.asList("Orange", "Orange", "Orange"));
+		BigDecimal expectedCost = new BigDecimal(0.50).setScale(2, RoundingMode.HALF_UP);
+
+		assertEquals(expectedCost, cart.calculateCost());
 	}
 
 }
